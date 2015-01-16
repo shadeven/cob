1aefa104cb2f4d08e0444c5a93f76a049647211f
Steven Wu <steven.j.wu@gmail.com>
2015-01-17 09:39:47 +1030

    Added Javadoc.

--- src/main/java/au/com/shadeven/development/singleton/SingletonClass.java
+++ src/main/java/au/com/shadeven/development/singleton/SingletonClass.java
@@ -1,11 +1,30 @@
 package au.com.shadeven.development.singleton;
 
+/**
+ * The class returns a singleton instance. However, it might not work in a
+ * multi-thread environment.
+ * 
+ * @author Steven
+ *
+ */
 public class SingletonClass {
 
+	/**
+	 * 
+	 */
 	private static final SingletonClass INSTANCE = new SingletonClass();
-	
-	private SingletonClass() {}
-	
+
+	/**
+	 * The private constructor that prevents instantiation from outside.
+	 */
+	private SingletonClass() {
+	}
+
+	/**
+	 * The static method returns the static instance.
+	 * 
+	 * @return
+	 */
 	public static SingletonClass getInstance() {
 		return INSTANCE;
 	}
